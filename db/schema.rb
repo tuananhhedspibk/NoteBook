05d7d0629e6dc0fe504c5f406556d549c27b1317
tuananhhedspibk <tuananhhedspibk1@gmail.com>
2017-04-08 11:03:35 +0700

    Add association between user and note

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170408035716) do
+ActiveRecord::Schema.define(version: 20170408035902) do
 
   create_table "notes", force: :cascade do |t|
     t.string   "title"
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
